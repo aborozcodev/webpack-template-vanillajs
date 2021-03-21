e7827fc25c0fd7507fec925d035cfbc69ef97ced
Adrian Bautista <adrianbautistaorozco@gmail.com>
2021-03-20 23:00:55 -0500

    Webpack devtools added

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
     entry: './src/index.js',
@@ -12,6 +13,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]' 
     },
     mode: 'development',
+    devtool: "source-map",
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -69,7 +71,8 @@ module.exports = {
                 to: "assets/images"
             }]
         }),
-        new Dotenv()
+        new Dotenv(),
+        new BundleAnalyzerPlugin()
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
